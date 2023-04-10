d13f3f97408b3f477de28b32292cfce71c7bc485
sabravo <belloaugustine@gmail.com>
2023-04-10 10:48:52 +0100

    fix error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,77 @@
-# Use Puppet to automate the task of creating a custom HTTP header response
+#manifest that setup a custom HTTP header for web server
 
-exec {'update':
-  command => '/usr/bin/apt-get update',
+package {'nginx':
+  ensure   => 'installed',
+  provider => 'apt',
 }
--> package {'nginx':
+
+exec {'allow http':
+  command => '/usr/sbin/ufw allow "Nginx HTTP"'
+}
+
+file {'/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!'
+}
+
+file {'/var/www/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page"
+}
+
+file_line {'redirect':
   ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'root /var/www/html',
+  line   => 'rewrite ^/redirect_me https://www.digitalocean.com/community permanent;',
 }
--> file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+
+file {'/etc/nginx/sites-enabled/default':
+  ensure  => file,
+  content => "add_header X-Served-By ${facts['facts['networking']['hostname']']}"
 }
--> exec {'run':
-  command => '/usr/sbin/service nginx restart',
+
+exec {'nginx restart':
+  command  => 'service nginx restart',
+  provider => 'shell',
+  require  => Package['nginx']
+}
+#manifest that setup a custom HTTP header for web server
+
+package {'nginx':
+  ensure   => 'installed',
+  provider => 'apt',
+}
+
+exec {'allow http':
+  command => '/usr/sbin/ufw allow "Nginx HTTP"'
+}
+
+file {'/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!'
+}
+
+file {'/var/www/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page"
+}
+
+file_line {'redirect':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'root /var/www/html',
+  line   => 'rewrite ^/redirect_me https://www.digitalocean.com/community permanent;',
+}
+
+file {'/etc/nginx/sites-enabled/default':
+  ensure  => file,
+  content => "add_header X-Served-By ${facts['facts['networking']['hostname']']}"
+}
+
+exec {'nginx restart':
+  command  => 'service nginx restart',
+  provider => 'shell',
+  require  => Package['nginx']
 }
 
